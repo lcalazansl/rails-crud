05b0084f7db27f6df4a35055cda9f86c151587d6
Lucas Luz <lucas@calazansluz.com>
2023-04-02 10:12:13 +0200

    update restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,14 @@ class RestaurantsController < ApplicationController
     restaurant.save
     redirect_to restaurant_path(restaurant)
   end
-
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
   private
 
   def restaurant_params
