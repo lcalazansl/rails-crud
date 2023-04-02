e262aac03915005ae0f60d5eaccbfeabdbdef85c
Lucas Luz <lucas@calazansluz.com>
2023-04-02 14:11:04 +0200

    refactoring

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only:[:show, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.all
   end
   def show
     # raise
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant = Restaurant.find(params[:id])
   end
   def new
     @restaurant = Restaurant.new
@@ -15,19 +16,22 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(restaurant)
   end
   def edit
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant = Restaurant.find(params[:id])
   end
   def update
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant)
   end
   def destroy
-    @restaurant = Restaurant.find(params[:id])
+    # @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path, status: :see_other
   end
   private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :rating, :address)
